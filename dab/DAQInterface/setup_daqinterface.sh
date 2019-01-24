2db8d8552a1a475fd263a99718273fb2378a9c7b
David Rivera <drivera@fnal.gov>
2019-01-24 15:26:51 -0600

    Working version of the fhicl files for dab setup, tested with fixes by Ron et al. Also added check for local setup_sbn_artdaq.sh.

--- dab/DAQInterface/setup_daqinterface.sh
+++ dab/DAQInterface/setup_daqinterface.sh
@@ -2,7 +2,17 @@
 
 THIS_SBN_DAQ_DAQINTERFACE_DIR=$(pwd)
 
-source $THIS_SBN_DAQ_DAQINTERFACE_DIR/setup_sbn_artdaq_local.sh
+#default setup file is:
+SETUP_FILE=$THIS_SBN_DAQ_DAQINTERFACE_DIR/setup_sbn_artdaq.sh
+LOCAL_SETUP_FILE=$THIS_SBN_DAQ_DAQINTERFACE_DIR/setup_sbn_artdaq_local.sh
+
+#if a local setup file exists, use that instead
+if [ -f $LOCAL_SETUP_FILE ]; then
+    SETUP_FILE=$LOCAL_SETUP_FILE
+fi
+
+echo "Setting up with : $LOCAL_SETUP_FILE"
+source $SETUP_FILE
 export DAQINTERFACE_USER_SOURCEFILE=$THIS_SBN_DAQ_DAQINTERFACE_DIR/user_sourcefile
 source $ARTDAQ_DAQINTERFACE_DIR/source_me
 
--- dab/DAQInterface/setup_sbn_artdaq_local.sh
+++ dab/DAQInterface/setup_sbn_artdaq_local.sh
@@ -2,12 +2,18 @@
 source /software/products/setup
 setup sbndaq v0_01_03 -q e15:prof
 setup mrb
-source /home/nfs/icaza/sbndaq_devel/localProducts*/setup
+
+#your own local products directory:
+LOCAL_PRODUCTS="/home/nfs/sbnd/work.drivera/srcs/sbndaq/v00_01_03/localProducts_sbndaq_v0_01_03_e15_prof/"
+
+source $LOCAL_PRODUCTS/setup
 unsetup -j artdaq_daqinterface
 setup artdaq_daqinterface v3_03_01T111318
 
 
-export TRACE_FILE=/tmp/trace_buffer
+#Trace setup for debugging:
+#export TRACE_FILE=/tmp/trace_buffer
+export TRACE_FILE=/proc/trace/buffer
 # export TRACE_LIMIT_MS="5,1000,2000" 
 
 # toffM 15 -n CommandableFragmentGenerator

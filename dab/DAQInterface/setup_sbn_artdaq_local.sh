76dd067bf9d519d4f6df1dff241f8865cb884fc4
William Badgett <badgett@icarus-gateway01.fnal.gov>
2019-02-04 17:24:07 -0600

    re-order setups

--- dab/DAQInterface/setup_sbn_artdaq_local.sh
+++ dab/DAQInterface/setup_sbn_artdaq_local.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
-
 source /software/products/setup
-setup sbndaq v0_01_03 -q e15:prof
 setup mrb
 
-#your own local products directory (HACK):
-LOCAL_PRODUCTS="$(pwd)/../../../../localProducts*/"
+#your own local products directory:
+LOCAL_PRODUCTS="${HOME}/work/localProducts_sbndaq_v0_01_03_debug_e15"
 
 source $LOCAL_PRODUCTS/setup
-unsetup -j artdaq_daqinterface
+#unsetup -j artdaq_daqinterface
+setup sbndaq v0_01_03 -q e15:debug
+
 setup artdaq_daqinterface v3_03_01T111318
 
 
 #Trace setup for debugging:
 #export TRACE_FILE=/tmp/trace_buffer
-export TRACE_FILE=/proc/trace/buffer
+export TRACE_FILE=/tmp/trace_wb
+#export TRACE_FILE=/proc/trace/buffer
 # export TRACE_LIMIT_MS="5,1000,2000" 
 
 # toffM 15 -n CommandableFragmentGenerator

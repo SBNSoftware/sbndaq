6d63d1d57facccbd65d3285f8caf22234bab11be
Iker Loïc de Icaza Astiz <i.de-icaza-astiz@sussex.ac.uk>
2019-02-13 20:28:28 -0600

    Corrections and additions to the setup files

--- dab/DAQInterface/setup_daqinterface.sh
+++ dab/DAQInterface/setup_daqinterface.sh
@@ -11,8 +11,9 @@ if [ -f $LOCAL_SETUP_FILE ]; then
     SETUP_FILE=$LOCAL_SETUP_FILE
 fi
 
-echo "Setting up with : $LOCAL_SETUP_FILE"
+echo "Setting up with : $SETUP_FILE"
 source $SETUP_FILE
+
 export DAQINTERFACE_USER_SOURCEFILE=$THIS_SBN_DAQ_DAQINTERFACE_DIR/user_sourcefile
 source $ARTDAQ_DAQINTERFACE_DIR/source_me
 

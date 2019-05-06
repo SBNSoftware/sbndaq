436cac183649bba5ea45760069ce5e00231721cf
William Badgett <badgett@sbnd-test02.fnal.gov>
2019-05-06 14:12:59 -0500

    use e17

--- dab/DAQInterface/setup_sbn_artdaq.sh
+++ dab/DAQInterface/setup_sbn_artdaq.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source /software/products/setup
-setup sbndaq v0_01_07 -q e15:prof
+setup sbndaq v0_01_07 -q e17:prof
 unsetup -j artdaq_daqinterface
 setup artdaq_daqinterface v3_03_01T111318rc

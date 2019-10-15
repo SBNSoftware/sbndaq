fa57873e232ae7883274a869250f9b01a7b6f9d8
Gennadiy <lukhanin@fnal.gov>
2019-10-15 13:23:14 -0500

    Upgrade to artdaq v3_06_02, bump sbndaq to v0_03_00

--- dab/DAQInterface/setup_sbn_artdaq.sh
+++ dab/DAQInterface/setup_sbn_artdaq.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 source /software/products/setup
-setup sbndaq v0_02_06 -q e17:prof
+[[ -f /software/products_dev/setup ]] && source /software/products_dev/setup
+
+setup sbndaq v0_03_00 -q e17:prof:s82
 unsetup -j artdaq_daqinterface
-setup artdaq_daqinterface v3_06_00
+setup artdaq_daqinterface v3_06_02
+
--- sbn-fd/DAQInterface/setup_sbn_artdaq.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq.sh
@@ -2,7 +2,6 @@
 source /software/products/setup
 [[ -f /software/products_dev/setup ]] && source /software/products_dev/setup
 
-setup sbndaq v0_02_06 -q e17:prof:s82
+setup sbndaq v0_03_00 -q e17:prof:s82
 unsetup -j artdaq_daqinterface
-
-setup artdaq_daqinterface v3_06_01_FB23262
+setup artdaq_daqinterface v3_06_02

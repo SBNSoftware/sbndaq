81640e5a6f0770c743e014b41349848182a2b563
Gennadiy <lukhanin@fnal.gov>
2019-12-13 19:42:42 -0600

    Upgrade to artdaq_v3_07_01, bump version to v0_04_00

--- sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -7,15 +7,16 @@ setup mrb
 
 THIS_SBN_DAQ_DAQINTERFACE_DIR=$(dirname "${BASH_SOURCE[0]}")
 THIS_SBN_DAQ_DAQINTERFACE_DIR=$(realpath "${THIS_SBN_DAQ_DAQINTERFACE_DIR}")
-SBNDAQ_VERSION="v0_03_00"
+SBNDAQ_VERSION="v0_04_00"
 SBNDAQ_QUAL1="e17"
 SBNDAQ_QUAL2="prof"
-SBNDAQ_QUAL3="s82"
+SBNDAQ_QUAL3="s83"
 
 # your own local products directory:
 LOCAL_PRODUCTS_TRY="${THIS_SBN_DAQ_DAQINTERFACE_DIR}/../../../../localProducts_sbndaq_${SBNDAQ_VERSION}_${SBNDAQ_QUAL1}_${SBNDAQ_QUAL2}_${SBNDAQ_QUAL3}"
 LOCAL_PRODUCTS_TRY2="${HOME}/work/sbndaq*/localProducts_sbndaq_${SBNDAQ_VERSION}_${SBNDAQ_QUAL1}_${SBNDAQ_QUAL2}_${SBNDAQ_QUAL3}"
-#LOCAL_PRODUCTS_TRY3="$(tlp="$( ls $(dirname $(pwd))/*/setup |grep local |head -1 )" && [[ -n $tlp ]] && echo  $(dirname $tlp)|| echo "null")"
+#LOCAL_PRODUCTS_TRY3="$(dirname $(pwd))/products"
+#"$(tlp="$( ls $(dirname $(pwd))/*/setup |grep local |head -1 )" && [[ -n $tlp ]] && echo  $(dirname $tlp)|| echo "null")"
 
 if   [ -e $LOCAL_PRODUCTS_TRY/ ]; then
    LOCAL_PRODUCTS=$LOCAL_PRODUCTS_TRY
@@ -32,8 +33,10 @@ fi
 [[ -f $LOCAL_PRODUCTS/setup ]] &&  source $LOCAL_PRODUCTS/setup
 setup sbndaq $SBNDAQ_VERSION -q ${SBNDAQ_QUAL1}:${SBNDAQ_QUAL2}:${SBNDAQ_QUAL3}
 
-setup artdaq_daqinterface v3_06_02
 export ARTDAQ_DATABASE_CONFDIR=/data/artdaq_database/config
+#unset DAQINTERFACE_STANDARD_SOURCEFILE_SOURCED
+setup artdaq_daqinterface v3_07_01
+
 
 #Trace setup for debugging:
 export TRACE_FILE=/tmp/trace_`whoami`

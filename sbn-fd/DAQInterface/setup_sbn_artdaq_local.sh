7f9be658cf59a42c86f75ee2f98894de3dd7754e
Gennadiy <lukhanin@fnal.gov>
2020-03-31 16:55:39 -0500

    Upgrade configs to artdaq v3_08_00

--- sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -2,7 +2,7 @@
 
 SBNDAQ_VERSION="v0_06_00"
 SBNDAQ_QUALS="e19:prof:s94:py2"
-DAQINTERFACE_VERSION="v3_07_02CH11c7c4" 
+DAQINTERFACE_VERSION="v3_08_00" 
 
 source /daq/software/products/setup
 [[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup

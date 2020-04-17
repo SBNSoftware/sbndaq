0b664309d89612ebece91c36cf3371bd4ca5f764
William Badgett <badgett@sbn-daq01.fnal.gov>
2020-04-17 11:12:18 -0500

    1st pass sbn-nd

---
+++ sbn-nd/DAQInterface/setup_sbn_artdaq.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+source /daq/software/products/setup
+[[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
+
+SBNDAQ_VERSION="v0_05_01"
+SBNDAQ_QUALS="e19:prof:s94:py2"
+DAQINTERFACE_VERSION="v3_07_02" 
+
+setup sbndaq $SBNDAQ_VERSION -q $SBNDAQ_QUALS
+setup artdaq_daqinterface $DAQINTERFACE_VERSION

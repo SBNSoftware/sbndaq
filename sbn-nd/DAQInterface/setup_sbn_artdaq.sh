fb3ffc8dfb538f5baf9fa44dbf304008bf2962b8
Sbnd <sbnd@sbnd-tpc02.fnal.gov>
2020-07-10 13:44:55 -0500

    Working configs for tpc02

--- sbn-nd/DAQInterface/setup_sbn_artdaq.sh
+++ sbn-nd/DAQInterface/setup_sbn_artdaq.sh
@@ -4,7 +4,7 @@ source /daq/software/products/setup
 
 SBNDAQ_VERSION="v0_06_01"
 SBNDAQ_QUALS="e19:prof:s94:py2"
-DAQINTERFACE_VERSION="v3_08_00" 
+DAQINTERFACE_VERSION="v3_08_00"
 
 setup sbndaq $SBNDAQ_VERSION -q $SBNDAQ_QUALS
 setup artdaq_daqinterface $DAQINTERFACE_VERSION
--- sbn-nd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-nd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -2,7 +2,7 @@
 
 SBNDAQ_VERSION="v0_06_01"
 SBNDAQ_QUALS="e19:prof:s94:py2"
-DAQINTERFACE_VERSION="v3_08_00" 
+DAQINTERFACE_VERSION="v3_08_00FB24312FB24420"
 
 source /daq/software/products/setup
 [[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup

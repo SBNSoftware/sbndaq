5f7b262d1777906e89667ed8f69a132a4fabf597
Gennadiy <lukhanin@fnal.gov>
2020-07-29 19:50:03 -0500

    CRT configuration used for tests on Jul 29, 2020, using artdaq8

--- sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-SBNDAQ_VERSION="v0_06_01"
+SBNDAQ_VERSION="v0_07_00"
 SBNDAQ_QUALS="e19:prof:s94:py2"
-#DAQINTERFACE_VERSION="v3_08_00"
-DAQINTERFACE_VERSION="v3_08_00FB24312FB24420"
+DAQINTERFACE_VERSION="v3_09_00"
 
 source /daq/software/products/setup
 [[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
+[[ -f /daq/software/products_experimental/setup ]] && source /daq/software/products_experimental/setup
 
 setup mrb
 
@@ -31,7 +31,7 @@ unset DAQINTERFACE_STANDARD_SOURCEFILE_SOURCED
 setup artdaq_daqinterface $DAQINTERFACE_VERSION
 
 setup artdaq_runcontrol_gui v1_00_01 -q e19:prof
-setup artdaq_mfextensions  v1_04_01BF24393 -q $SBNDAQ_QUALS
+#setup artdaq_mfextensions  v1_04_02 -q $SBNDAQ_QUALS
 
 alias rc='artdaqRunControl'
 
@@ -47,6 +47,7 @@ tonSg 0-2
 #tonMg 0-3
 tonM 0-63 -n BernCRT_GeneratorBase
 toffM 6,11 -n BernCRT_GeneratorBase 
+tonM 0-63 -N*CommandableFragment*
 #toffM 23 -n SharedMemoryManager
 tmodeS 1
 tmodeM 1

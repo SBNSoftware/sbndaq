22391cd60eac1cb3591f991151592443744f1ae8
Gennadiy <lukhanin@fnal.gov>
2020-08-13 21:36:20 -0500

    Upgrade to artdaq v3_09_01

--- for-jenkins/build-all-projects.sh
+++ for-jenkins/build-all-projects.sh
@@ -5,15 +5,15 @@ PRODUCTS=${PRODUCTS:-'/cvmfs/fermilab.opensciencegrid.org/products/artdaq:/cvmfs
 #configure variables
 working_dir="${WORKSPACE:-$(pwd)}"
 selected_build_config=${1:-${BUILDCONFIG:-"testing"}}
-qual_set="${2:-${QUAL:-'s94:py:e19'}}"
+qual_set="${2:-${QUAL:-'s94:py2:e19'}}"
 build_type="${3:-${BUILDTYPE:-'prof'}}"
 PROJECT_SOURCE_GIT_PREFIX="${4:-${PROJECT_SOURCE_GIT_PREFIX:-'https://github.com/sbnsoftware'}}"
 export PROJECT_SOURCE_GIT_PREFIX
 
-ARTDAQ_VERSION=${ARTDAQ_VERSION:-"v3_08_00"}
+ARTDAQ_VERSION=${ARTDAQ_VERSION:-"v3_09_01"}
 export ARTDAQ_VERSION
 
-DEFAULT_BRANCHTAG=${DEFAULT_BRANCHTAG:-'v0_06_01'}
+DEFAULT_BRANCHTAG=${DEFAULT_BRANCHTAG:-'v0_07_00'}
 PRODUCTS=$(for d in $(echo $PRODUCTS | tr ":" " "); do [[ -d $d ]] && echo -n "$d:"; done)
 PRODUCTS=${PRODUCTS::-1}
 export PRODUCTS
@@ -26,7 +26,7 @@ export WORKSPACE
 
 #available configurations
 source_branchtages_testing=(
-   "wibtools:v0_03_04"
+   "wibtools:v0_03_06"
    "sbndaq_artdaq_core:${SBNDAQ_ARTDAQ_CORE_BRANCHTAG:-${DEFAULT_BRANCHTAG}}"
    "sbndaq_artdaq:${SBNDAQ_ARTDAQ_BUILDTAG:-${DEFAULT_BRANCHTAG}}"
    "sbndaq:${SBNDAQ_BRANCHTAG:-${DEFAULT_BRANCHTAG}}"

2505da7f0f36c1c890dbf0fb9ace1e760df7714a
Gennadiy <lukhanin@fnal.gov>
2020-08-17 17:42:53 -0500

    Update running configs

--- sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -29,7 +29,7 @@ export ARTDAQ_DATABASE_CONFDIR=/daq/software/database/config
 unset DAQINTERFACE_STANDARD_SOURCEFILE_SOURCED
 setup artdaq_daqinterface $DAQINTERFACE_VERSION
 
-setup artdaq_runcontrol_gui v1_00_01 -q e19:prof
+setup artdaq_runcontrol_gui v1_01_00 -q e19:prof
 setup artdaq_mfextensions  v1_05_00 -q $SBNDAQ_QUALS
 
 alias rc='artdaqRunControl'

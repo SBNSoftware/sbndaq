5be7c008282eb6f328f911a66fe040ab9e0a4c23
Gennadiy <lukhanin@fnal.gov>
2020-08-18 21:03:19 -0500

    Bump daqinterface to v3_09_01

--- sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -2,7 +2,7 @@
 
 SBNDAQ_VERSION="v0_07_00"
 SBNDAQ_QUALS="e19:prof:s94:py2"
-DAQINTERFACE_VERSION="v3_09_00"
+DAQINTERFACE_VERSION="v3_09_01"
 
 source /daq/software/products/setup
 [[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup

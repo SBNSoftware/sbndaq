c2d732540e6f287c566dafbe0774722800791092
Gennadiy <lukhanin@fnal.gov>
2020-09-30 13:11:52 -0500

    WIP: tests 093020

--- sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -4,6 +4,9 @@ SBNDAQ_VERSION="v0_07_00"
 SBNDAQ_QUALS="e19:prof:s94:py2"
 DAQINTERFACE_VERSION="v3_09_01"
 
+unset PRODUCTS
+unset DAQINTERFACE_TRACE_SCRIPT
+
 source /daq/software/products/setup
 [[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
 

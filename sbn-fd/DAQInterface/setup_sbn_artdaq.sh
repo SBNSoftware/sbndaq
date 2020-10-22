60324ba3d09b9b3d59b1b8be186aef41e8b154e5
Gennadiy <lukhanin@fnal.gov>
2020-10-21 19:55:22 -0500

    Upgrade to artdaq v3_09_02

--- sbn-fd/DAQInterface/setup_sbn_artdaq.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq.sh
@@ -2,8 +2,8 @@
 source /daq/software/products/setup
 [[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
 
-SBNDAQ_VERSION="v0_07_01"
-SBNDAQ_QUALS="e19:prof:s94:py2"
+SBNDAQ_VERSION="v0_07_02"
+SBNDAQ_QUALS="e19:prof:s97:py2"
 DAQINTERFACE_VERSION="v3_09_02" 
 
 setup sbndaq $SBNDAQ_VERSION -q $SBNDAQ_QUALS
--- sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SBNDAQ_VERSION="v0_07_01"
-SBNDAQ_QUALS="e19:prof:s94:py2"
+SBNDAQ_QUALS="e19:prof:s97:py2"
 DAQINTERFACE_VERSION="v3_09_02"
 
 unset PRODUCTS

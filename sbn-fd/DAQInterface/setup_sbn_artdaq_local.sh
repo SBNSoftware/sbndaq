9c25a9fee3451a0bcbd01ebfb6053206b3dc944d
Gennadiy <lukhanin@fnal.gov>
2020-10-27 12:39:46 -0500

    everything working. cleanup default configs on the EVBs a bit, including only sending to dispather from evbs 1-5

--- sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-fd/DAQInterface/setup_sbn_artdaq_local.sh
@@ -33,7 +33,7 @@ export ARTDAQ_DATABASE_CONFDIR=/daq/software/database/config
 unset DAQINTERFACE_STANDARD_SOURCEFILE_SOURCED
 setup artdaq_daqinterface $DAQINTERFACE_VERSION
 
-setup artdaq_runcontrol_gui v1_01_00 -q e19:prof
+#setup artdaq_runcontrol_gui v1_01_02 -q e19:prof
 setup artdaq_mfextensions  v1_05_02 -q $SBNDAQ_QUALS
 
 alias rc='artdaqRunControl'

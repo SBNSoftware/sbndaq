3051fe23c9334c5637c4c316e882ba6d45015862
Gennadiy <lukhanin@fnal.gov>
2021-10-19 12:50:45 -0500

    Bump to v1_00_01

--- sbn-test/DAQInterface/setup_sbn_artdaq.sh
+++ sbn-test/DAQInterface/setup_sbn_artdaq.sh
@@ -2,7 +2,7 @@
 source /daq/software/products/setup
 [[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
 
-SBNDAQ_VERSION="v1_00_00"
+SBNDAQ_VERSION="v1_00_01"
 SBNDAQ_QUALS="e20:prof:s112"
 DAQINTERFACE_VERSION="v3_11_00" 
 

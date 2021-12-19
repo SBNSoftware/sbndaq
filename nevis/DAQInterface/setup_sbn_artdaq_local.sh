db7e89880d3d48e08d5387151ff65ab171c52fb2
Daisy Kalra <kalra@sbnd-gateway01.fnal.gov>
2021-12-18 23:25:47 -0600

    Replicated DAQInterface in nevis directory so as to make a link to DAQInterface

---
+++ nevis/DAQInterface/setup_sbn_artdaq_local.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+SBNDAQ_VERSION="v0_07_06"
+SBNDAQ_QUALS="e19:prof:s106"
+DAQINTERFACE_VERSION="v3_09_08"
+
+unset PRODUCTS
+unset DAQINTERFACE_TRACE_SCRIPT
+
+source /daq/software/products/setup
+#[[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
+#[[ -f /daq/software/products_experimental/setup ]] && source /daq/software/products_experimental/setup
+
+#setup mrb
+setup mrb v4_04_02
+
+THIS_SBN_DAQ_DAQINTERFACE_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+
+# your own local products directory:
+LOCAL_MRB_PRODUCTS=$(/bin/ls -d ${THIS_SBN_DAQ_DAQINTERFACE_DIR%srcs*}localProducts*${SBNDAQ_VERSION}* |head -1)
+LOCAL_INSTALLED_PRODUCTS=${THIS_SBN_DAQ_DAQINTERFACE_DIR%srcs*}products
+
+if   [ -f $LOCAL_INSTALLED_PRODUCTS/setup ]; then
+   source $LOCAL_INSTALLED_PRODUCTS/setup
+elif [ -f $LOCAL_MRB_PRODUCTS/setup ]; then
+   source $LOCAL_MRB_PRODUCTS/setup
+else
+   echo "Warning: localProducts/products not found."
+fi
+
+setup sbndaq $SBNDAQ_VERSION -q ${SBNDAQ_QUALS}
+
+export ARTDAQ_DATABASE_CONFDIR=/daq/software/database/config
+unset DAQINTERFACE_STANDARD_SOURCEFILE_SOURCED
+setup artdaq_daqinterface $DAQINTERFACE_VERSION
+
+#setup artdaq_runcontrol_gui v1_02_00_01 -q e19:prof
+setup artdaq_mfextensions  v1_05_05 -q $SBNDAQ_QUALS
+
+alias rc='artdaqRunControl'
+
+#Trace setup for debugging:
+export TRACE_FILE=/tmp/trace_$(whoami)_p1
+
+echo "TRACE_FILE=$TRACE_FILE"
+
+#suppress debug messages
+toffSg 8-63
+tonSg 0-7
+tonMg 0-7
+tmodeS 1
+tmodeM 1
+
+
+#toffM 23 -n SharedMemoryManager
+#export TRACE_LIMIT_MS="5,1000,2000"
+# toffM 15 -n CommandableFragmentGenerator
+# tonM 15 -n CommandableFragmentGenerator 
+
+tonM 0-63 -n CommandableFragmentGenerator
+tonM 0-63 -N NevisTPC* 
+tonS 24 -N NevisTPCGenerator*
+toffM debug+6 -N NevisTPC*     #debug is level8, turning off level 8+6 here.

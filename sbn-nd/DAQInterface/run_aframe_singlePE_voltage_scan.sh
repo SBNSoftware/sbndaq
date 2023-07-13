a8d7a92b545ece40b3ae42dac0d4c6c41a12d9e5
sbnd <sbnd@fnal.gov>
2023-07-13 18:17:56 -0500

    Initiate github repo for single PE measurement.

--- sbn-nd/DAQInterface/run_aframe_singlePE_voltage_scan.sh
+++ sbn-nd/DAQInterface/run_aframe_singlePE_voltage_scan.sh
@@ -3,9 +3,10 @@
 # the script for single PE search data taking for A-frame, each run is about 2 hours. 
 
 # enlarge the PULL window. 
-cp configs/standard/aframe_singlePE_voltage_scanning/crt01PULL_singlePE.fcl configs/standard/crt01PULL.fcl
+cp configs/standard/aframe_singlePE_voltage_scanning/crt01PULL_singlePE.fcl configs/standard/crt_aframePULL.fcl
 
-for i in 140 170 200 230 
+echo "=============== This set of runs starts ===============" >> ./runlog_bias_scan.log
+for i in 140 160 180 200 220 
 do
         echo "configs/standard/aframe_singlePE_voltage_scanning/crt01_aframe_singlePE_${i}.fcl" >> ./runlog_singlePE_search.log
         cp configs/standard/aframe_singlePE_voltage_scanning/crt01_aframe_singlePE_${i}.fcl configs/standard/crt01.fcl
@@ -20,7 +21,7 @@ done
 
 # put everything back to normal. 
 cp configs/standard/aframe_singlePE_voltage_scanning/crt01.fcl.copy configs/standard/crt01.fcl
-cp configs/standard/aframe_singlePE_voltage_scanning/crt01PULL.fcl.copy configs/standard/crt01PULL.fcl
+cp configs/standard/aframe_singlePE_voltage_scanning/crt_aframePULL.fcl.copy configs/standard/crt_aframePULL.fcl
 
 echo "=============== This set of runs stops ===============" >> ./runlog_bias_scan.log
 echo " " >> ./runlog_bias_scan.log 

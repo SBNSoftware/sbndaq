752f0f2fa6a90c1f7c797faf2dddeaee3e6cc8c7
Gennadiy Lukhanin <glukhanin@solitonica.com>
2024-02-14 14:47:44 -0600

    Update config files for using in the test area

--- sbn-test/DAQInterface/setup_sbn_artdaq.sh
+++ sbn-test/DAQInterface/setup_sbn_artdaq.sh
@@ -1,15 +1,66 @@
 #!/usr/bin/env bash
-source /daq/software/products/setup
-[[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
+SBNDAQ_VERSION="v1_09_00"
+SBNDAQ_QUALS="e26:prof:s120a"
+DAQINTERFACE_VERSION="v3_12_08" 
+MFEXTENSIONS_VERSION="v1_08_06"
+ARTDAQDEMO_VERSION="v3_12_07" 
 
-SBNDAQ_VERSION="v1_08_06"
-SBNDAQ_QUALS="e20:prof:s120a"
-DAQINTERFACE_VERSION="v3_12_07"
-
-setup sbndaq $SBNDAQ_VERSION -q $SBNDAQ_QUALS
-setup artdaq_daqinterface $DAQINTERFACE_VERSION
 
 # reconfigure locale
 export LANG='en_US.UTF-8'
 export LC_TIME='en_US.UTF-8'
 export LC_ALL='en_US.UTF-8'
+
+THIS_SBN_DAQ_DAQINTERFACE_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+EXTRA_PRODUCTS_DIR=$(echo $THIS_SBN_DAQ_DAQINTERFACE_DIR | awk -F'/srcs/' '{print $1}')/products
+
+source /daq/software/products/setup
+
+echo EXTRA_PRODUCTS_DIR=$EXTRA_PRODUCTS_DIR
+
+[[ -f ${EXTRA_PRODUCTS_DIR}/setup ]] && export PRODUCTS=${EXTRA_PRODUCTS_DIR}:$PRODUCTS
+#[[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
+
+#source /nvme_scratch/products1/setup
+
+setup sbndaq $SBNDAQ_VERSION -q $SBNDAQ_QUALS
+
+[ -n "${ARTDAQDEMO_VERSION+x}" ] && [ -n "$ARTDAQDEMO_VERSION" ] &&\
+  setup artdaq_demo $ARTDAQDEMO_VERSION -q $SBNDAQ_QUALS 
+
+export ARTDAQ_DATABASE_ENV="${HOME}/.artdaq_database-sbndaq${SBNDAQ_VERSION}.env"
+
+unset DAQINTERFACE_STANDARD_SOURCEFILE_SOURCED
+setup artdaq_daqinterface $DAQINTERFACE_VERSION
+
+[ -n "${MFEXTENSIONS_VERSION+x}" ] && [ -n "$MFEXTENSIONS_VERSION" ] &&\
+ setup artdaq_mfextensions $MFEXTENSIONS_VERSION -q $SBNDAQ_QUALS
+
+#Trace setup for debugging:
+export DAQINTERFACE_PARTITION_NUMBER=${DAQINTERFACE_PARTITION_NUMBER:-"1"}
+
+export TRACE_FILE=/tmp/trace_$(whoami)_p${DAQINTERFACE_PARTITION_NUMBER}
+
+echo "TRACE_FILE=$TRACE_FILE"
+
+ups active | grep  -E "(^sbn|^wib|^artdaq_runcontrol_gui|^caen)"
+
+toffSg 0-63
+toffMg 0-63
+tonSg 0-8
+tonMg 0-8
+
+if [[ "$(hostname -s)" =~ icarus-crt11 ]]; then
+    echo Checking if the boardreader is running. If not, attempting to kill the Bottom CRT backend
+    if ! /usr/sbin/pidof boardreader ; then
+        if /usr/sbin/pidof bottomCRTreadout ; then
+            echo Attempting to kill the backend
+            /usr/bin/killall bottomCRTreadout
+        else
+            echo Backend not running, no need to kill it
+        fi
+    else
+        echo Boardreader is running, refraining from killing the backend
+    fi
+fi
+
--- sbn-test/DAQInterface/setup_sbn_artdaq_local.sh
+++ sbn-test/DAQInterface/setup_sbn_artdaq_local.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-
-SBNDAQ_VERSION="v1_08_06"
-SBNDAQ_QUALS="e20:prof:s120a"
+SBNDAQ_VERSION="v1_09_00"
+SBNDAQ_QUALS="e26:prof:s120a"
 DAQINTERFACE_VERSION="v3_12_07"
 
 unset PRODUCTS
 unset DAQINTERFACE_TRACE_SCRIPT
 
+# reconfigure locale
+export LANG='en_US.UTF-8'
+export LC_TIME='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
+
 source /daq/software/products/setup
-[[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
+#[[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
 #[[ -f /daq/software/products_experimental/setup ]] && source /daq/software/products_experimental/setup
 
 setup mrb v6_08_01
@@ -28,31 +32,35 @@ else
 fi
 
 setup sbndaq $SBNDAQ_VERSION -q ${SBNDAQ_QUALS}
-setup artdaq_demo v3_12_07 -q ${SBNDAQ_QUALS}
 
 export ARTDAQ_DATABASE_ENV="${HOME}/.artdaq_database-sbndaq${SBNDAQ_VERSION}.env"
 
 unset DAQINTERFACE_STANDARD_SOURCEFILE_SOURCED
 setup artdaq_daqinterface $DAQINTERFACE_VERSION
 
-setup artdaq_mfextensions  v1_08_06 -q $SBNDAQ_QUALS
+setup artdaq_mfextensions   v1_08_06 -q $SBNDAQ_QUALS
 
 alias rc='artdaqRunControl'
 
-# reconfigure locale
-export LANG='en_US.UTF-8'
-export LC_TIME='en_US.UTF-8'
-export LC_ALL='en_US.UTF-8'
 
 #Trace setup for debugging:
-export TRACE_FILE=/tmp/trace_$(whoami)_p1
+export TRACE_FILE=/tmp/trace_$(whoami)_p7
 
 echo "TRACE_FILE=$TRACE_FILE"
 
-#suppress debug messages
-toffSg 8-63
-tonSg 0-7
-tonMg 0-7
+#start with turning off everything
+toffSg 0-63
+toffMg 0-63
+
+#turn on what we need
+tonSg 0-8     # ton* does not suppress other levels
+tonMg 0-8
+
+#if you want to see debug++ messages, ton levels 9 and/or above
+#S - slow path and message viewer
+#M - memory trace
+
+#toffSg 9-63   # suppress debug++ messages to Slow path
 #toffM 1-63 -n PhysCrateData
 #toffS 1-63 -n PhysCrateData
 
@@ -60,12 +68,51 @@ tonMg 0-7
 #tonM 1-63 -n PhysCrate_GeneratorBase
 #tonM 9-15 -n CAENV1730Readout
 
+#turning everything in PMT on
+#tonM 0-63 -n CAENV1730Readout
+
+
 #tonS 0-debug -n ICARUSTriggerUDP
 tmodeS 1
 tmodeM 1
+
+#Examples to turn on/off messages coming from specific machines and specific classes
+
+#if [[ "$(hostname -s)" =~ icarus-evb[0-9]{2} ]]; then
+#toffM -N *_SharedMemoryEventManager 0-63
+#toffM -N ArtdaqSharedMemoryService 0-63
+#toffM -N SharedMemoryEventReceiver 0-63
+#toffM -N SharedMemoryManager 0-63
+#toffM -N *RootDAQOut 0-63
+#toffM -N MetricManager 0-63
+#toffM -N TCPConnect 0-63
+#toffM -N EventBuilder1_art1_FragmentWatcher 0-63
+#tonM -N EventBuilder1_art1_FragmentWatcher DEBUG+1
+#tonS -N EventBuilder1_art1_FragmentWatcher DEBUG+1
+#fi
+#
+#if [[ "$(hostname -s)" =~ icarus-pmt[0-9]{2} ]]; then
+#tonS -N *CAENV1730Readout  9
+#tonM -N *CAENV1730Readout  9
+#fi
 
 
 #toffM 23 -n SharedMemoryManager
 #export TRACE_LIMIT_MS="5,1000,2000"
 # toffM 15 -n CommandableFragmentGenerator
 # tonM 15 -n CommandableFragmentGenerator
+
+
+if [[ "$(hostname -s)" =~ icarus-crt11 ]]; then
+    echo Checking if the boardreader is running. If not, attempting to kill the Bottom CRT backend
+    if ! /usr/sbin/pidof boardreader ; then
+        if /usr/sbin/pidof bottomCRTreadout ; then
+            echo Attempting to kill the backend
+            /usr/bin/killall bottomCRTreadout
+        else
+            echo Backend not running, no need to kill it
+        fi
+    else
+        echo Boardreader is running, refraining from killing the backend
+    fi
+fi

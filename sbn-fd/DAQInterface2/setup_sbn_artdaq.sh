d13c599fc9ea22d249c2fcf7b4bb1ba092e2abba
Gennadiy Lukhanin <glukhanin@solitonica.com>
2024-07-31 20:01:08 -0500

    Restore the workaround for crt11

--- sbn-fd/DAQInterface2/setup_sbn_artdaq.sh
+++ sbn-fd/DAQInterface2/setup_sbn_artdaq.sh
@@ -6,6 +6,7 @@ export ARTDAQ_DAQINTERFACE_VERSION=v3_13_00
 export ARTDAQ_MFEXTENSIONS_VERSION=v1_09_00
 export BUILD_VARIANT='gcc@12.2.0'
 
+export DAQINTERFACE_CONFIGURE_STATUS_CHECK_QUIET_TIME=60
 
 export SPACK_DISABLE_LOCAL_CONFIG=true
 
@@ -19,7 +20,6 @@ export SPACK_INSTALL_DIR=/daq/software/spack_packages/spack/current/NULL
 [[ -f ${SPACK_INSTALL_DIR}/share/spack/setup-env.sh ]] || { echo "Error: Not a Spack installation"; return 10; }
 
 source ${SPACK_INSTALL_DIR}/share/spack/setup-env.sh >/dev/null 2>&1
-#source ${THIS_SBN_DAQ_DAQINTERFACE_DIR}/setup-env.sh >/dev/null 2>&1
 
 which spack >/dev/null 2>&1 || { echo "Error: Spack not setup"; return 10; }
 
@@ -44,15 +44,14 @@ export DAQINTERFACE_USER_SOURCEFILE=${THIS_SBN_DAQ_DAQINTERFACE_DIR}/user_source
 source package_setup.sh "sbndaq-suite@${SBNDAQ_VERSION}%${BUILD_VARIANT}"  2>/dev/null  
 
 export PYTHONPATH=$(echo "$PYTHONPATH" | awk -v RS=':' -v ORS=':' '!seen[$0]++' | sed 's/:$//')
-export ARTDAQ_DATABASE_VERSION=$(spack find artdaq-database arch=${SPACK_ARCH}| grep artdaq-database |grep -Eo 'v[0-9_]+')
+export ARTDAQ_DATABASE_VERSION=$(spack find artdaq-database arch=${SPACK_ARCH} 2>/dev/null | grep artdaq-database |grep -Eo 'v[0-9_]+')
 
 spack find -lf --loaded 2>&1 | grep -v 'Warning: Missing dependency not in database:' 
 
 export ARTDAQ_DATABASE_CONFDIR=/daq/software/database/config
 
 #Trace setup for debugging:
-source $(which trace_functions.sh 2>/dev/null) \
-  || echo "trace_functions.sh not found in PATH."
+source $(which trace_functions.sh 2>/dev/null) { echo "trace_functions.sh not found in PATH."; return 12; }
 
 export TRACE_FILE=/tmp/trace_$(whoami)_p1
 echo "TRACE_FILE=$TRACE_FILE"
@@ -61,9 +60,25 @@ export PYTHONUNBUFFERED=TRUE
 
 
 #suppress debug messages
-toffSg 8-63
-toffMg 8-63
-tonSg 0-7
-tonMg 0-7
+#
+toffSg 0-63
+toffMg 0-63
+tonSg 0-8
+tonMg 0-8
+
 tmodeS 1
 tmodeM 1
+
+if [[ "$(hostname -s)" =~ icarus-crt11 ]]; then
+    echo Checking if the boardreader is running. If not, attempting to kill the Bottom CRT backend
+    if ! /usr/sbin/pidof boardreader ; then
+        if /usr/sbin/pidof bottomCRTreadout ; then
+            echo Attempting to kill the backend
+            /usr/bin/killall bottomCRTreadout
+        else
+            echo Backend not running, no need to kill it
+        fi
+    else
+        echo Boardreader is running, refraining from killing the backend
+    fi
+fi

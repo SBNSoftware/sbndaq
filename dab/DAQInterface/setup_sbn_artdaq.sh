c71d70e2b4e6dcc2ed135ae590e9e413a301b0da
Gennadiy Lukhanin <lukhanin@fnal.gov>
2025-02-27 16:42:50 -0600

    Bump to v1_10_06

--- dab/DAQInterface/setup_sbn_artdaq.sh
+++ dab/DAQInterface/setup_sbn_artdaq.sh
@@ -2,7 +2,7 @@
 source /daq/software/products/setup
 #[[ -f /daq/software/products_dev/setup ]] && source /daq/software/products_dev/setup
 
-SBNDAQ_VERSION="v1_10_05"
+SBNDAQ_VERSION="v1_10_06"
 SBNDAQ_QUALS="e26:prof:s131"
 DAQINTERFACE_VERSION="v3_13_00"
 

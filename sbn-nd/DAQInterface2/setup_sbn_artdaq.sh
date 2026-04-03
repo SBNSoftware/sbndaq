7d59a857ca5f3abde94223d867924cf5f1588bba
Gennadiy Lukhanin <lukhanin@fnal.gov>
2026-04-03 01:04:39 -0500

    Update hashes

--- sbn-nd/DAQInterface2/setup_sbn_artdaq.sh
+++ sbn-nd/DAQInterface2/setup_sbn_artdaq.sh
@@ -7,8 +7,8 @@ SBNDAQ_VERSION='v2_01_00'
 BUILD_VARIANT='gcc@13.1.0'
 
 declare -A build_hash_map=(
-    [scientific7]="/ovwxtpz"
-    [almalinux9]="/lqtb5oa"
+    [scientific7]="/6d4rvf2"
+    [almalinux9]="/t3nduhk"
 )
 
 USE_CACHED_BASH_ENV=True
